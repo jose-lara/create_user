3c2902e1e818aaf9176cd2275755cfd4e9846c83
Jose Lara <jose.laradelamo@axa.com>
2019-11-13 19:06:41 +0100

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-'use strict';
+const AWS = require("aws-sdk");
+const uuidv1 = require("uuid/v1");
 
-var fs = require('fs');
-var path = require('path');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
-
-  callback(null, result);
+const handlerFunction = async (event, context, callback) => {
+  console.info({ event });
+  const { userName, userSurname, role } =
+    typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+  const id = uuidv1();
+  try {
+    const options = {
+      TableName: "user",
+      Item: { id, active: true, userName, userSurname, role }
+    };
+    await docClient.put(options).promise();
+    const result = {
+      statusCode: 201,
+      body: JSON.stringify(options.Item),
+      headers: { "content-type": "application/json" }
+    };
+    callback(null, result);
+  } catch (e) {
+    return context.fail(e);
+  }
 };
+exports.handler = handlerFunction;
