ea6ef8dc8a9ba4a635245587048fa14f294e0cea
Jose Lara <jose.laradelamo@axa.com>
2019-11-13 19:10:34 +0100

    fix template data

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const AWS = require("aws-sdk");
-const uuidv1 = require("uuid/v1");
+const AWS = require('aws-sdk');
+const uuidv1 = require('uuid/v1');
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 const handlerFunction = async (event, context, callback) => {
-  console.info({ event });
-  const { userName, userSurname, role } =
-    typeof event.body === "string" ? JSON.parse(event.body) : event.body;
-  const id = uuidv1();
+  const { userName, userSurname, role } = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
+  const userId = uuidv1();
   try {
     const options = {
-      TableName: "user",
-      Item: { id, active: true, userName, userSurname, role }
+      TableName: 'User',
+      Item: { userId, active: true, userName, userSurname, role }
     };
     await docClient.put(options).promise();
     const result = {
       statusCode: 201,
       body: JSON.stringify(options.Item),
-      headers: { "content-type": "application/json" }
+      headers: { 'content-type': 'application/json' }
     };
     callback(null, result);
   } catch (e) {
